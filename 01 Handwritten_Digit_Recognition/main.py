941f27fac3806875c56c5a8559415bfa78a72bb7
singh-jagjot <jagjot.no1@gmail.com>
2023-08-26 19:33:30 -0400

    Optimization Added

--- 01 Handwritten_Digit_Recognition/main.py
+++ 01 Handwritten_Digit_Recognition/main.py
@@ -1,5 +1,6 @@
 import putil
 import numpy as np
+import pickle
 import gzip, sys
 import matplotlib.pyplot as plt
 
@@ -35,25 +36,73 @@ def get_test_data():
     labels = get_label_data('./t10k-labels-idx1-ubyte.gz')
     return images, labels
 
+def get_accuracy(y:np.ndarray, y_hat:np.ndarray):
+    return np.sum(y == y_hat)/ len(y) * 100
+
+def save_data(data, file_name):
+    with open(file_name, 'wb') as file:
+        pickle.dump(data, file)
+
+def load_data(file_name):
+    with open(file_name, 'rb') as file:
+        data = pickle.load(file)
+    return data
 
 def main():
-    x_train, y_train = get_train_data(2)
-    outputs_n = 10
+    x_train, y_train = get_train_data()
+    # Number of classes
+    c = 10
     # plt.imshow(x_train[2])
     # plt.show()
     # print(labels[2])
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1] * x_train.shape[2]))
-    # print(x_train.shape)
-    # print(y_train.shape)
-    W = np.random.random((x_train.shape[1], outputs_n))
-    b = np.random.random(outputs_n)
-    # print(W.shape, b.shape)
+    x_train = x_train / 255.0
+    m, n = x_train.shape
+    np.random.seed(10)
+    W = np.random.random((n, c))
+    np.random.seed(10)
+    b = np.random.random(c)
+
     model = putil.Softmax()
-    # model.fit(x_train, y_train, np.zeros((784, 10)), np.ones(10))
     model.fit(x_train, y_train, W, b)
-    model._get_cost()
-    model._get_gradient()
-    # model.fit(np.arange(12).reshape((-1,3)), np.arange(4), np.zeros((3,4)), np.array([2,4,6,8]))
-    # model._get_f_wb()
+    # model.normalize_data()
+    model.optimize(1, 2000)
+
+    data = model.get_weights_bias()
+    FILE_NAME = 'data0.pkl'
+    save_data(data, FILE_NAME)
+    # W2, b2 = load_data(FILE_NAME)
+    # model2 = putil.Softmax()
+    # model2.set_weight(W2)
+    # model2.set_bias(b2)
+    # print(model._get_cost(), m._get_cost())
+    # mu, sigma = model.get_normailizing_data()
+    # x_train = (x_train - mu) / sigma
+    x_test, y_test = get_test_data()
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1] * x_test.shape[2]))
+    x_test = x_test / 255.0
+    # x_test = (x_test - mu) / sigma
+
+    train_predictions = model.predict(x_train)
+    test_predictions = model.predict(x_test)
+    train_accuracy = get_accuracy(y_train, train_predictions)
+    test_accuracy = get_accuracy(y_test, test_predictions)
+    print("Train Accuracy: {:.3f}%\nTest Accuracy: {:.3f}%".format(train_accuracy, test_accuracy))
+
+def main2():
+    FILE_NAME = 'data0.pkl'
+    W, b = load_data(FILE_NAME)
+    model = putil.Softmax()
+    model.set_weight(W)
+    model.set_bias(b)
+
+    x_test, y_test = get_test_data()
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1] * x_test.shape[2]))
+    x_test = x_test / 255.0
+
+    test_predictions = model.predict(x_test)
+    test_accuracy = get_accuracy(y_test, test_predictions)
+    print('Accuracy: ', test_accuracy, "%")
 if __name__ == '__main__':
-    main()
+    # main()
+    main2()
--- 01 Handwritten_Digit_Recognition/putil.py
+++ 01 Handwritten_Digit_Recognition/putil.py
@@ -5,45 +5,60 @@ import numpy as np
 
 class Softmax:
     def __init__(self):
-        self.X = None
-        self.y = None
-        self.W = None
-        self.b = None
+        self.X: np.ndarray = None
+        self.y: np.ndarray = None
+        self.y_hot: np.ndarray = None
+        self.W: np.ndarray = None
+        self.b: np.ndarray = None
+        self.mu :None
+        self.sigma = None
 
     def set_weight(self, W):
-        self.W = W
+        self.W: np.ndarray = W
 
     def set_bias(self, b):
-        self.b = b
+        self.b: np.ndarray = b
 
     def fit(self, X, y):
-        self.X = X
-        self.y = y
+        self.X: np.ndarray = X
+        self.y: np.ndarray = y
+        self._one_hot_enc()
 
     def fit(self, X, y, W, b):
-        self.W = W
-        self.b = b
-        self.X = X
-        self.y = y
+        self.W: np.ndarray = W
+        self.b: np.ndarray = b
+        self.X: np.ndarray = X
+        self.y: np.ndarray = y
+        self._one_hot_enc()
+
+    def get_weights_bias(self):
+        '''return -> W, b'''
+        return self.W, self.b
 
     def _softmax(self, z):
         # Stable Softmax (prevents overflow)
-        X = self.X
-        z = z - np.max(z, axis=1).reshape((X.shape[0], 1))
+        z = z - np.max(z, axis=1).reshape((z.shape[0], 1))
         ez = np.exp(z)
         ez_sum = np.sum(ez, axis=1).reshape((-1, 1))
-        return np.divide(ez, ez_sum)
+        return ez / ez_sum
 
-    def _get_f_wb(self):
-        X, W, b = self.X, self.W, self.b
-        # Weights are in columns as w1 in column 1, w2 in column 2 and so on.
-        z = np.matmul(X, W) + b
-        # print(z)
-        f_wb = self._softmax(z)
-        return f_wb
+    # def _get_f_wb(self):
+    #     X, W, b = self.X, self.W, self.b
+    #     # Weights are in columns as w1 in column 1, w2 in column 2 and so on.
+    #     z = np.matmul(X, W) + b
+    #     # print(z)
+    #     f_wb = self._softmax(z)
+    #     # print('fwb',f_wb)
+    #     return f_wb
+    def _one_hot_enc(self):
+        _, c = self.W.shape
+        y_hot = np.zeros((len(self.y), c))
+        y_hot[np.arange(len(self.y)), self.y] = 1
+        self.y_hot = y_hot
+        return y_hot
 
     def _get_cost(self):
-        X, W, b,y = self.X, self.W, self.b,self.y
+        X, W, b, y = self.X, self.W, self.b, self.y
         # Unfortunately below code won't work because of high possibility of log(0) errors.
         # t = time.time()
         # f_wb = self._get_f_wb()
@@ -52,39 +67,71 @@ class Softmax:
         # cost = np.sum(j_wb)
         # print('t1', time.time() - t)
 
-        # Slow code
-        # t = time.time()
-        # cost2 = 0
-        # for i,val in enumerate(y):
-        #     f_wb_i = f_wb[i][val]
-        #     cost2 += -np.log(f_wb_i)
-        # print('t2', time.time() - t)
-        # print(cost, cost2)
-
-        # print('cost',cost)
         # Below code works without log(0) errors
         # Weights are in columns as w1 in column 1, w2 in column 2 and so on.
         z = np.matmul(X, W) + b
-        z_max = np.max(z, axis=1).reshape((X.shape[0], 1))
-        z_new = z-z_max
+        z_max = np.max(z, axis=1).reshape((z.shape[0], 1))
+        z_new = z - z_max
         ez_new = np.exp(z_new)
         ez_sum = np.sum(ez_new, axis=1).reshape((-1, 1))
         # print('z',z)
+        # print('y', y)
         # print('z_max',z_max)
         # print('z_new',z_new)
         # print('ez_new',ez_new)
         # print('ez_sum',ez_sum)
-        log_softmax = (z -z_max) - np.log(ez_sum)
+        log_softmax = z_new - np.log(ez_sum)
+        # print(log_softmax)
         loss = -log_softmax[np.arange(y.shape[0]), y]
-        cost = np.sum(loss)
-        print('cost', cost)
+        # print(loss)
+        cost = np.mean(loss)
+        # print('cost', cost)
         return cost
 
-    def _get_gradient(self, alpa=0):
+    def _get_gradient(self):
+        X, y, W, b, y_hot = self.X, self.y, self.W, self.b, self.y_hot
+        z = np.matmul(X, W) + b
+        f_wb = self._softmax(z)
+        y_hat = f_wb[np.arange(y.shape[0]), y]
+        m, n = X.shape
+
+        dj_dw = np.matmul(X.T, f_wb - y_hot) / m
+        dj_db = np.sum(f_wb - y_hot, axis=0) / m
+        # print(f_wb.shape)
+        # print(y_hot.shape)
+        # print(f_wb.shape)
+        # print(W.shape)
+        # print(b.shape)
+        # print(dj_dw)
+        # print(dj_db)
+        return dj_dw, dj_db
+
+    def _gradient_descent(self, alpha, epochs):
         # 'alpa' is learning rate
-        W, b = self.W, self.b
-        y = self.y
-        dj_dw = self._get_f_wb() - 1
-        dj_db = self._get_f_wb() - 1
-        print(dj_dw, dj_db)
-        return None
+        for epoch in range(epochs):
+            dj_dw, dj_db = self._get_gradient()
+            self.W -= alpha * dj_dw
+            self.b -= alpha * dj_db
+            # print(dj_db)
+            if (epoch + 1) % 10 == 0 or epoch + 1 == epochs or epoch == 0:
+                print("Epoch {}, cost: {}".format(epoch+1, self._get_cost()))
+
+    def optimize(self, alpha, epochs):
+        self._gradient_descent(alpha=alpha, epochs=epochs)
+
+    def predict(self, X):
+        z = np.matmul(X, self.W) + self.b
+        f_wb = self._softmax(z)
+        return np.argmax(f_wb, axis=1)
+
+    def normalize_data(self):
+        X = self.X
+        mu = np.mean(X)
+        sigma = np.std(X)
+        self.X = (X - mu) / sigma
+        self.mu = mu
+        self.sigma = sigma
+
+    def get_normailizing_data(self):
+        '''return -> mu, sigma'''
+        return self.mu, self.sigma
